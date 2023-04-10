e7a401aa509a4502466635f4b2b7edb9ff085f6f
Michael <124548904+bamchip@users.noreply.github.com>
2023-04-10 11:18:20 -0700

    :tada: Initial commit

---
+++ news_aggregator_data_access_layer/__init__.py
@@ -0,0 +1,14 @@
+"""A cool news aggregator data access layer project."""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
