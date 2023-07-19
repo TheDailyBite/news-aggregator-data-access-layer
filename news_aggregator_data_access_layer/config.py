b35206a21f0cc03bcda75d04df85ff017401474b
Michael <124548904+bamchip@users.noreply.github.com>
2023-07-18 21:59:07 -0700

    Changed default region to us-east-1

--- news_aggregator_data_access_layer/config.py
+++ news_aggregator_data_access_layer/config.py
@@ -2,7 +2,7 @@ import os
 
 DEPLOYMENT_STAGE = os.environ.get("DEPLOYMENT_STAGE", "dev")
 DYNAMODB_HOST = os.environ.get("DYNAMODB_HOST", None)
-REGION_NAME = os.environ.get("REGION_NAME", "us-west-1")
+REGION_NAME = os.environ.get("REGION_NAME", "us-east-1")
 DEFAULT_NAMESPACE = os.environ.get("DEFAULT_NAMESPACE", "NewsAggregatorDataAccessLayer")
 LOCAL_TESTING = os.environ.get("LOCAL_TESTING", "false").lower() in ["true"]
 CANDIDATE_ARTICLES_S3_BUCKET = os.environ.get(
